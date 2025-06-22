f9a9fb69b7ec6a9492b67ea55a3ae49f31bbef88
JayadevArun <jayadevarun03@gmail.com>
2025-06-22 21:32:20 +0530

    adding header, nav and nav link schema

---
+++ frontend/src/shared/components/Navigation/NavLinks.js
@@ -0,0 +1,29 @@
+import React,{useContext} from 'react'
+import {NavLink} from 'react-router-dom'
+
+
+import './NavLinks.css'
+
+const NavLinks = (props) => {
+
+  return (
+    <ul className='nav-links'>
+        <li>
+            <NavLink to="/" exact>ALL USERS</NavLink>
+        </li>
+            <li>
+            </li>
+            <li>
+                <NavLink to="/places/new">ADD PLACE</NavLink>
+            </li>
+            <li>
+                <NavLink to="/auth">AUTHENTICATE</NavLink>
+            </li>
+            <li>
+                <button onClick={auth.logout}>LOGOUT</button>
+            </li>
+    </ul>
+  )
+}
+
+export default NavLinks

84afc2530798b78082187152613c457b548821b7
JayadevArun <jayadevarun03@gmail.com>
2025-06-25 15:58:49 +0530

    http error handling, db initialized

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,13 @@
 const express=require('express');
 const bodyParser=require('body-parser');
+const mongoose=require('mongoose');
 
 const path=require('path');
 
 const placesRoutes=require('./routes/places-routes');
 const usersRoutes=require('./routes/users-routes');
+const HttpError=require('./models/http-error');
+
 
 const app=express();
 
@@ -24,6 +27,10 @@ app.use((req,res,next)=>{
 app.use('/api/places',placesRoutes);
 app.use('/api/users',usersRoutes);
 
+app.use((req,res,next)=>{
+    const error=new HttpError('Could not find this route.',404);
+    throw error;
+});
 
 app.use((error,req,res,next)=>{
     if(res.headerSent){
@@ -33,3 +40,11 @@ app.use((error,req,res,next)=>{
     res.json({message: error.message || 'An unknown error has occured.'});
 });
 
+mongoose
+    .connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.htxjbgd.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority&appName=Cluster0`)
+    .then(()=>{
+        app.listen(5000);
+    })
+    .catch(err=>{
+        console.log(err);
+    });
